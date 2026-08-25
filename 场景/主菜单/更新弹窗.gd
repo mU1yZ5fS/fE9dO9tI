8505cd26487d2ee853608356655f61f17550bd9f
mU1yZ5fS <227023811+mU1yZ5fS@users.noreply.github.com>
2026-08-25 17:27:53 +0800

    敏感信息删除

--- 场景/主菜单/更新弹窗.gd
+++ 场景/主菜单/更新弹窗.gd
@@ -1,6 +1,5 @@
 extends PopupPanel
 ## 更新弹窗：仅从 GitHub 静态托管拉取版本号与更新内容。
-## 二维码改为游戏内置本地图片，不进行任何在线维护；
 ## 不显示任何可能跳转到 GitHub 仓库的按钮。
 
 # 当前游戏本地版本，发布新版本时同步修改。
@@ -12,26 +11,13 @@ const UPDATE_URL_PRIMARY := "https://mU1yZ5fS.github.io/fE9dO9tI/update.json"
 # raw 地址作为备用，公开仓库不需要先开启 Pages 也能访问。
 const UPDATE_URL_FALLBACK := "https://raw.githubusercontent.com/mU1yZ5fS/fE9dO9tI/main/docs/update.json"
 
-# 本地内置二维码（替换此文件即可更新游戏内二维码，不经过网络）。
-const QR_TEXTURE := preload("res://资产/UI/更新/qq_group.png")
-
 @onready var _title: Label = $布局/标题
 @onready var _status: Label = $布局/状态
 @onready var _announcement: Label = $布局/公告
 @onready var _changelog: RichTextLabel = $布局/日志滚动/更新日志
-@onready var _qr: TextureRect = $布局/二维码区/二维码
-@onready var _qr_status: Label = $布局/二维码区/二维码说明/二维码状态
-@onready var _qq_name: Label = $布局/二维码区/二维码说明/群名称
 @onready var _close_btn: Button = $布局/按钮行/关闭
 
 var _used_fallback := false
-
-
-func _ready() -> void:
-	_close_btn.pressed.connect(close_popup)
-	_qr.texture = QR_TEXTURE
-	_qr_status.text = "游戏内置二维码"
-
 
 func open_update_popup() -> void:
 	_show_loading()
@@ -49,11 +35,6 @@ func _show_loading() -> void:
 	_status.text = "正在检查更新…"
 	_announcement.text = ""
 	_changelog.text = "正在连接更新服务器…"
-	_qr.texture = QR_TEXTURE
-	_qr_status.text = "游戏内置二维码"
-	_qq_name.text = "QQ交流群"
-
-
 func _fetch_update() -> void:
 	_used_fallback = false
 	_request_json(UPDATE_URL_PRIMARY)
